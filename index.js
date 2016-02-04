376c4443a77d763c0f8277772ad3407a3d842634
Mathias Buus <mathiasbuus@gmail.com>
2016-02-04 01:47:35 +0100

    use from2

--- index.js
+++ index.js
@@ -1,68 +1,42 @@
-var fs = require('fs')
 var from = require('from2')
-var through = require('through2')
+var fs = require('fs')
 var path = require('path')
-var pump = require('pump')
-var stream = require('readable-stream')
-var util = require('util')
 
-module.exports = Walker
+module.exports = walker
 
-function Walker (dir, opts) {
-  var self = this
-  if (!(this instanceof Walker)) return new Walker(dir, opts)
-  if (!opts) opts = {}
-  self._dir = dir
-  self._want = true
-  stream.Readable.call(this, {objectMode: true, highWaterMark: 16})
-  self.filter = opts.filter || function (filename) { return true }
-}
+function walker (dir, opts) {
+  var filter = opts && opts.filter || function (filename) { return true }
+  var pending = filter(dir) ? [dir] : []
 
-util.inherits(Walker, stream.Readable)
+  return from.obj(read)
 
-Walker.prototype._read = function () {
-  var self = this
-  if (!self._want) return
-  self._want = false
-  fs.stat(self._dir, function (err, st) {
-    if (err) return self.emit('error', err)
-    if (!st.isDirectory()) return self._onfile(self._dir, done)
-    self._walk(self._dir, done)
-  })
+  function read (size, cb) {
+    if (!pending.length) return cb(null, null)
 
-  function done (err) {
-    if (err) return self.emit('error', err)
-    self._end()
+    var name = pending.shift()
+    fs.lstat(name, function (err, st) {
+      if (err) return done(err)
+      if (!st.isDirectory()) return done(null)
+
+      fs.readdir(name, function (err, files) {
+        if (err) return done(err)
+        files.sort()
+        for (var i = 0; i < files.length; i++) {
+          var next = path.join(name, files[i])
+          if (filter(next)) pending.unshift(next)
+        }
+        done(null)
+      })
+
+      function done (err) {
+        if (err) return cb(err)
+        cb(null, {
+          basename: path.basename(name),
+          relname: dir === name ? path.basename(name) : path.relative(dir, name),
+          filepath: name,
+          stat: st
+        })
+      }
+    })
   }
-}
-
-Walker.prototype._walk = function (dir, cb) {
-  var self = this
-  fs.readdir(dir, function (err, files) {
-    if (err) return cb(err)
-    var walk = through.obj(function (data, enc, next) {
-      self._onfile(path.join(dir, data), next)
-    })
-    pump(from.obj(files.sort()), walk, cb)
-  })
-}
-
-Walker.prototype._onfile = function (filepath, cb) {
-  var self = this
-  if (!this.filter(filepath)) return cb()
-  fs.lstat(filepath, function (err, stat) {
-    if (err) return cb(err)
-    self.push({
-      basename: path.basename(filepath),
-      relname: self._dir === filepath ? path.basename(filepath) : path.relative(self._dir, filepath),
-      filepath: filepath,
-      stat: stat
-    })
-    if (stat.isDirectory()) return self._walk(filepath, cb)
-    return cb()
-  })
-}
-
-Walker.prototype._end = function () {
-  this.push(null)
 }
